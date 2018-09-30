7ab0bee054a2e69715094ec374feebc2322f119b
John  Tucker <john@larkintuckerllc.com>
2018-09-30 15:08:31 -0400

    RenderProp

--- App.js
+++ App.js
@@ -1,82 +1,37 @@
 import React, { Component } from 'react';
-import { Animated, Dimensions, Keyboard, StyleSheet, TextInput, UIManager } from 'react-native';
-
-const { State: TextInputState } = TextInput;
+import { StyleSheet, TextInput, View } from 'react-native';
+import KeyboardShift from './KeyboardShift';
 
 export default class App extends Component {
-  state = {
-    shift: new Animated.Value(0),
-  };
-
-  componentWillMount() {
-    this.keyboardDidShowSub = Keyboard.addListener('keyboardDidShow', this.handleKeyboardDidShow);
-    this.keyboardDidHideSub = Keyboard.addListener('keyboardDidHide', this.handleKeyboardDidHide);
-  }
-
-  componentWillUnmount() {
-    this.keyboardDidShowSub.remove();
-    this.keyboardDidHideSub.remove();
-  }
-
   render() {
-    const { shift } = this.state;
     return (
-      <Animated.View style={[styles.container, { transform: [{translateY: shift}] }]}>
-        <TextInput
-          placeholder="A"
-          style={styles.textInput}
-        />
-        <TextInput
-          placeholder="B"
-          style={styles.textInput}
-        />
-        <TextInput
-          placeholder="C"
-          style={styles.textInput}
-        />
-        <TextInput
-          placeholder="D"
-          style={styles.textInput}
-        />
-        <TextInput
-          placeholder="E"
-          style={styles.textInput}
-        />
-      </Animated.View>
+      <KeyboardShift>
+        {() => (
+          <View style={styles.container}>
+            <TextInput
+              placeholder="A"
+              style={styles.textInput}
+            />
+            <TextInput
+              placeholder="B"
+              style={styles.textInput}
+            />
+            <TextInput
+              placeholder="C"
+              style={styles.textInput}
+            />
+            <TextInput
+              placeholder="D"
+              style={styles.textInput}
+            />
+            <TextInput
+              placeholder="E"
+              style={styles.textInput}
+            />
+          </View>
+        )}
+      </KeyboardShift>
     );
-  }
-
-  handleKeyboardDidShow = (event) => {
-    const { height: windowHeight } = Dimensions.get('window');
-    const keyboardHeight = event.endCoordinates.height;
-    const currentlyFocusedField = TextInputState.currentlyFocusedField();
-    UIManager.measure(currentlyFocusedField, (originX, originY, width, height, pageX, pageY) => {
-      const fieldHeight = height;
-      const fieldTop = pageY;
-      const gap = (windowHeight - keyboardHeight) - (fieldTop + fieldHeight);
-      if (gap >= 0) {
-        return;
-      }
-      Animated.timing(
-        this.state.shift,
-        {
-          toValue: gap,
-          duration: 1000,
-          useNativeDriver: true,
-        }
-      ).start();
-    });
-  }
-
-  handleKeyboardDidHide = () => {
-    Animated.timing(
-      this.state.shift,
-      {
-        toValue: 0,
-        duration: 1000,
-        useNativeDriver: true,
-      }
-    ).start();
   }
 }
 
